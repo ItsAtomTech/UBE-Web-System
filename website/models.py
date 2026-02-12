3654e7dcf87f56e3b9f181eb85ffd18a4dd847f5
Weniit <nowaywow90@gmail.com>
2026-02-12 22:00:56 +0800

    check daw pre an models.py

--- website/models.py
+++ website/models.py
@@ -22,5 +22,29 @@ class Users(db.Model, UserMixin):
     def get_id(self):
         return str(self.user_id)  # Flask-Login will use user_id instead of default
 
+class Student_Table(db.Model):
+    student_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    student_number = db.Column(db.String(50))
+    current_section = db.Column(db.String(50))
+    year_level = db.Column(db.String(50))
+    program = db.Column(db.String(50))
+    date = db.Column(db.DateTime(timezone=True), default=manila_time)
+
+class Status_Record(db.Model):
+    record_id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student_table.student_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    status_id = db.Column(db.Integer, db.ForeignKey('academic_status.status_id'))
+    reason = db.Column(db.String(100))
+    date = db.Column(db.DateTime(timezone=True), default=manila_time)
+
+class Academic_Status(db.Model):
+    status_id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student_table.student_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    academic_status = db.Column(db.String(50))
+
+
 
 
