89b3777fc2fd2059b19f4762a62d34c5714836eb
Atom Energy <justmerrivera@gmail.com>
2026-02-17 10:31:27 +0800

    Applied some styles to the Login and Signin Input fields

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", ssl_context=('certs/cert.pem', 'certs/key.pem'))
+    app.run(debug=True, host="0.0.0.0", ssl_context=('certs/cert.pem', 'certs/key.pem'), port="5002")
