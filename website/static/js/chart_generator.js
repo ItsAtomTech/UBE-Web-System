94cb6e5af984d7bc7fd239cce038e8a57342f04c
Weniit <nowaywow90@gmail.com>
2026-03-12 08:32:27 +0800

    fix chart data values

--- website/static/js/chart_generator.js
+++ website/static/js/chart_generator.js
@@ -3,34 +3,34 @@ const datasets = [
   {
     label: "BLIS",
     data: [
-      ['2023 - 1st Sem', 60],
-      ['2023 - 2nd Sem', 120],
-      ['2024 - 1st Sem', 80],
-      ['2024 - 2nd Sem', 80],
-      ['2025 - 1st Sem', 80],
-      ['2025 - 2nd Sem', 60],
+      ['2023 - 1st Sem', 12],
+      ['2023 - 2nd Sem', 1],
+      ['2024 - 1st Sem', 23],
+      ['2024 - 2nd Sem', 43],
+      ['2025 - 1st Sem', 32],
+      ['2025 - 2nd Sem', 71],
     ]
   },
   {
     label: "BSIT",
     data: [
-      ['2023 - 1st Sem', 60],
-      ['2023 - 2nd Sem', 0],
-      ['2024 - 1st Sem', 40],
-      ['2024 - 2nd Sem', 40],
-      ['2025 - 1st Sem', 40],
-      ['2025 - 2nd Sem', 60],
+      ['2023 - 1st Sem', 31],
+      ['2023 - 2nd Sem', 43],
+      ['2024 - 1st Sem', 43],
+      ['2024 - 2nd Sem', 12],
+      ['2025 - 1st Sem', 76],
+      ['2025 - 2nd Sem', 83],
     ]
   },
   {
     label: "BEED",
     data: [
-      ['2023 - 1st Sem', null],
-      ['2023 - 2nd Sem', null],
-      ['2024 - 1st Sem', null],
-      ['2024 - 2nd Sem', null],
-      ['2025 - 1st Sem', 60],
-      ['2025 - 2nd Sem', 60],
+      ['2023 - 1st Sem', 12],
+      ['2023 - 2nd Sem', 43],
+      ['2024 - 1st Sem', 50],
+      ['2024 - 2nd Sem', 85],
+      ['2025 - 1st Sem', 32],
+      ['2025 - 2nd Sem', 10],
     ]
   },
   
